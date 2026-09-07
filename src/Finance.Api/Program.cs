bad1d28e8a8cf6c330566042d054cb3e3230aba5
Hamad Alaslani <hamadsmma@gmail.com>
2026-09-07 08:33:18 +0300

    wire up infrastructure DI and database connection string

--- src/Finance.Api/Program.cs
+++ src/Finance.Api/Program.cs
@@ -1,8 +1,14 @@
+using Finance.Infrastructure;
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -15,9 +21,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
---
+++ src/Finance.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+using Finance.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Finance.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
+    {
+        services.AddDbContext<FinanceDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("FinanceDb"))
+        );
+
+        return services;
+    }
+}
